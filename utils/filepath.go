c2c0735c88a7b9a0b604fbc7d9d8b30980c95d88
mjiulee <limj123@189.cn>
2019-10-22 10:07:27 +0800

    Fix utils.Pwd err in win

--- utils/filepath.go
+++ utils/filepath.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"path/filepath"
 	"runtime"
 )
 
@@ -10,6 +11,9 @@ var ostype = runtime.GOOS
 func GetPwd() string {
 	var projectPath string
 	projectPath, _ = os.Getwd()
+	if ostype == "windows" {
+		projectPath = filepath.ToSlash(projectPath)
+	}
 	return projectPath
 }
 
